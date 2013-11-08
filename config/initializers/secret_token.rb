b575e0ee31a5ca2fc97855a7a2607b85b3921521
irislee <irislee513@gmail.com>
2013-11-08 17:57:48 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmoebaCircus::Application.config.secret_key_base = '8c75165c74349f246d171641218ce59ab460b354204db4d48bdaac1042f9643f706e3469f6a1c19386e452935ea95c7638a2a715d6e6138119d84d3322f90d52'
